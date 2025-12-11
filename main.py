c78bfb6b29b6ff9d589a6d2119c27dc72011d82e
VextorHorizon <Coptersrichanth@gmail.com>
2025-12-11 21:44:48 +0700

    Finish port scanner script with hostname resolution
    Updated port scanner to resolve hostnames and improved argument handling.

--- main.py
+++ main.py
@@ -1,15 +1,30 @@
 import sys #lib that use to work with CLI
 import socket
 
-if len(sys.argv) != 2: #sys.argv read argument from CLI like "main.py[0] 192.168.1.1[1]" #2 mean it Argument not a tags
-    target = sys.argv[1]
-    #should use another command to transrate if scanner input use name host(www.google.com) instead of IP address(192.168.1.1)
+if len(sys.argv) == 2: #sys.argv read argument from CLI like "simpleportscanner.py[0] 192.168.1.1[1]" #2 mean it Argument not a tags
+    realtargetname = sys.argv[1]
+    target = socket.gethostbyname(sys.argv[1]) #input, use it in cli (simpleportscanner.py 192.168.1.1)
 else:
     print("Invild amount of Argument")
+    print("Syntax: python simpleportscanner.py <ip>")
+    sys.exit()
 
+print(f"Scanning {target} Port 1 to 1024 \n---------------------------------")
 try:
     for port in range(1,1024):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #socket.AF_INET is command use to connect to that IP address, socket.SOCK_STREAM is command to tell it to connect with TCP protocal
+        s.settimeout(0.5) #timeout
+
+        result = s.connect_ex((target, port)) #connect to target and port
+        if result == 0:
+            print(f"[+] Port {port} OPEN")
+    
+    sys.exit()
+
 
 except KeyboardInterrupt:
+    print("Interrupt by user")
+    sys.exit()
+except socket.gaierror:
+    print(f"Could not resolve the hostname {target}")
     sys.exit()
